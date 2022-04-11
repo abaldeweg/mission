d390e421e6fe7df452efca58403749ef58d6b73a
André Baldeweg <kontakt@andrebaldeweg.de>
2022-04-11 11:39:57 +0200

    fix test

--- parseJson/parseJson_test.go
+++ parseJson/parseJson_test.go
@@ -9,12 +9,12 @@ import (
 func TestConfig(t *testing.T) {
     blob := `{"notes":[],"replacements":{},"missions":[]}`
 	unmarshal := Read(blob)
-    if reflect.TypeOf(unmarshal).String() != "filetypes.Logfile" {
-        t.Fatalf("%s is not type of %s", reflect.TypeOf(unmarshal),  "filetypes.Logfile")
+    if reflect.TypeOf(unmarshal).String() != "parseJson.Logfile" {
+        t.Fatalf("%s is not type of %s", reflect.TypeOf(unmarshal),  "parseJson.Logfile")
     }
 
     marshal := Write(unmarshal)
-    if fmt.Sprintf("%s", reflect.ValueOf(marshal)) != `{"Notes":[],"Replacements":{},"Missions":[]}` {
-        t.Fatalf("%s is not type of %s", reflect.ValueOf(marshal),  "[]uint8")
+    if fmt.Sprintf("%s", reflect.ValueOf(marshal)) != `{"notes":[],"replacements":{},"missions":[]}` {
+        t.Fatalf("%s is not type of %s", reflect.ValueOf(marshal),  "interface{}")
     }
 }
